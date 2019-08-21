89c0e7298cec0f4bf86bafc468c15da0a7b253d4
unknown <pravinjadhav711@gmail.com>
2019-08-21 11:41:17 +0530

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,8 @@
+$('.slick', '.vertical-slider').slick({
+        vertical: true,
+        verticalSwiping: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots:true,
+        autoplay:false
+      });
